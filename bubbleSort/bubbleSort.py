227bfaf322ec1a53f6f3069ff124da1bbfa473a0
Lucas Prado <88468707+lucasprad05@users.noreply.github.com>
2022-08-22 18:22:41 -0300

    Update bubbleSort.py

---
+++ bubbleSort/bubbleSort.py
@@ -0,0 +1,25 @@
+#Bubble Sort
+#Lucas Prado & Erik Noda
+#Data Structures 2
+
+def bubbleSort(v, o):
+    change = True
+    n = len(v)
+    while change == True:
+        change = False
+        if o == 0:
+            for i in n:
+                if v[i] > v[i + 1]:
+                    v[i], v[i + 1] = v[i + 1], v[i]
+                    change = True
+                    print(v)
+        else:
+            for i in n:
+                if v[i] < v[i + 1]:
+                    v[i], v[i + 1] = v[i + 1], v[i]
+                    change = True
+                    print(v)
+
+o = input('Escolha a opção para escrever:\n 0 - Crescente\n 1 - Descrescente\n')
+v = [-19, -61, -36, 36, -48, 32, -9, 16, -34, -77, 80, -26, -18, -27, 84]
+bubbleSort(v, o)
