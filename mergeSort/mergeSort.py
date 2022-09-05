2dbbd20a4273d315268483d30734cc099bc3e941
Lucas Prado <88468707+lucasprad05@users.noreply.github.com>
2022-09-05 17:37:35 -0300

    Add files via upload

---
+++ mergeSort/mergeSort.py
@@ -0,0 +1,36 @@
+#Merge Sort
+#Lucas Prado & Erik Noda
+#Data Structures 2
+
+def mergeSort(array):
+    if len(array) > 1:
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+
+v = [-19, -61, -36, 36, -48, 32, -9, 16, -34, -77, 80, -26, -18, -27, 84]
+mergeSort(v)
+print(v)
